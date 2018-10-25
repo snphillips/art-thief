1629f1476c6caad9ca88b7e7f8834273355330b3
Sarah Phillips <snphillips@gmail.com>
2018-10-24 22:58:24 -0400

    lint

---
+++ client/build/precache-manifest.b767a595c678843c2db688c8790786ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3541b79fc3ed91e74b29",
+    "url": "/static/js/main.3541b79f.chunk.js"
+  },
+  {
+    "revision": "4710bd16dd718220f9d4",
+    "url": "/static/js/1.4710bd16.chunk.js"
+  },
+  {
+    "revision": "3541b79fc3ed91e74b29",
+    "url": "/static/css/main.6f8d22b0.chunk.css"
+  },
+  {
+    "revision": "bd81761fcf735f83c077b0ec16b5a90a",
+    "url": "/index.html"
+  }
+];
--- client/src/DropdownMenu.js
+++ client/src/DropdownMenu.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+// the spinner in an npm package
 import LoadingSpinner from './LoadingSpinner';
 
 export default class DropdownMenu extends Component {
@@ -8,27 +9,30 @@ export default class DropdownMenu extends Component {
     <form onSubmit={this.props.handleDropdownSubmit} >
 
       <select id="tag"
-
               value={this.props.parent_state.value}
               onChange={this.props.handleDropdownChange}>
 
-        <option value="industrial design">choose search keyword</option>
+        <option value="exoticism">choose search keyword</option>
         <option value="abstract">abstract</option>
         <option value="advertising">advertising</option>
         <option value="angular">angular</option>
         <option value="advertising">art nouveau</option>
         <option value="art deco">art deco</option>
-        <option value="floral">floral</option>
+        <option value="circles">circles</option>
         <option value="geometric">geometric</option>
+        <option value="industrial design">industrial design</option>
         <option value="modernism">modernism</option>
         <option value="pattern">pattern</option>
+        <option value="simple">simple</option>
         <option value="textile design">textile desgin</option>
 
       </select>
 
       <input type="submit"
              value="submit" />
+
     <LoadingSpinner loading={this.props.loading} />
+
     </form>
 
     );
