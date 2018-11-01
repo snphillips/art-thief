1fec215c76dc478d0e7229c13568302fa21aba0f
Sarah Phillips <snphillips@gmail.com>
2018-11-01 12:55:54 -0400

    tidy & add comments to server.js

--- server.js
+++ server.js
@@ -9,33 +9,35 @@ const app = express();
 
 const logger = require('morgan');
 
+// ==================================
+// Axios - npm package promise based HTTP client
+// ==================================
 const axios = require('axios');
 
-// **********************************
-// Redis & Response-time
-// npm packages
-// **********************************
-// const responseTime = require('response-time')
+// ==================================
+// Redis - npm package for cacheing
+// ==================================
 var redis = require("redis"),
-    // local hosting
-    // client = redis.createClient();
-    // Heroku hosting
     client = require('redis').createClient(process.env.REDIS_URL);
 
 
-// **********************************
+// ==================================
 // CORS
 // npm package to allow cross origin resource sharing
-// **********************************
+// ==================================
 const cors = require('cors')
 app.use(cors())
 
+// ==================================
 // Body-parser captures data coming via a form.
-// Allows our forms to work)
+// (Allows forms to work)
+// ==================================
 const bodyParser = require('body-parser');
 
 
-// set the port, either from an environmental variable or manually
+// ==================================
+// Set the port, either from an environmental variable or manually
+// ==================================
 const port = process.env.PORT || 8888;
 
 
@@ -49,9 +51,9 @@ app.get('/', (req, res, next) => {
 
 
 // **********************************
-// Get All - Search by Tag Redis
+// Get All - Search by Tag word user chooses
+// Redis caching taking place
 // **********************************
-
 const getItemBySearchTag = (req, res) => {
   let { value } = req.params;
   let url = `https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.search.objects&access_token=${process.env.COOPER_API_TOKEN}&has_images=1&per_page=200&tag=${value}`
@@ -62,10 +64,10 @@ const getItemBySearchTag = (req, res) => {
       console.log(`I have`, response.data.objects.length, `items for the keyword`, value, `.`)
         // SETEX is a redis command. It has 3 parameters: "key, seconds, value"
         // 1) key: Set the string-key:value in the cache.
-        // 2) seconds: Set set key to timeout after a given number of seconds (cache expiration) to 1 hour (60 minutes)
-        // 3600 = 1hr
+        // 2) seconds: Set set key to timeout after a given number of seconds (cache expiration).
+        // Currently set it for about 2 months....the data wont change much in 2 months.
         // 3) value: The response as JSON
-        client.setex(req.params.value, 172800, JSON.stringify(response.data));
+        client.setex(req.params.value, (60 * 60 * 24 * 7) * 8, JSON.stringify(response.data));
 
       return res.json(response.data)
     })
@@ -77,7 +79,9 @@ const getItemBySearchTag = (req, res) => {
 
 const getCache = (req, res) => {
 
-  //Check the cache data from the server redis
+  //  Check to see if there's already cached data(thank you redis)
+  //  If there is, return the result.
+  //  If not, then go get the data by hitting the cooper hewitt API
   client.get(req.params.value, (err, result) => {
     if (result) {
       res.send(result);
@@ -93,9 +97,9 @@ app.get('/searchbytag/:value', getCache);
 
 
 
-// **********************************
+// ==================================
 // Error Handlers
-// **********************************
+// ==================================
 app.use((err, req, res, next) => {
   res.json(err);
   res.status(500).send('Oh no a 500 error.')
@@ -105,22 +109,20 @@ app.use((req, res, next) => {
   res.status(404).send(`Oh no a 404 error. I can't find that.`)
 })
 
-
+// ==================================
+// Body-parser
+// npm package Parse incoming request bodies
+// in a middleware before handlers, using req.body property.
+// ==================================
 app.use(bodyParser.json());
 
-// **********************************
-// Middleware that adds a X-Response-Time
-// header to responses. --can't find at the moment
-// **********************************
-// app.use(responseTime());
 
-// **********************************
+// ==================================
 // Port
-// **********************************
+// ==================================
 app.listen(port, () => {
   console.log(`Let's steal some art! Listening on port: ${port}, in ${app.get('env')} mode.`);
 }).on('error', console.error);
-
 
 
 module.exports = app;
