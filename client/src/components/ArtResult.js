2b77f8f4d2841bd9cd037a956ca954a75529c22d
Sarah Phillips <snphillips@gmail.com>
2021-12-14 21:34:01 -0500

    refactor ArtResultfrom object to functional component

--- client/src/components/ArtResult.js
+++ client/src/components/ArtResult.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 
-export default class ArtResult extends Component {
-  render() {
+export default function ArtResult(props) {
+  return (
 
-    return (
-
-      <span className="ArtResult"
-            style={this.props.parent_state.displayArtResultImage}>
-
-        <img src={this.props.parent_state.imageURL}
-             alt="random item from Cooper Hewitt collection based on search tag"
-             className="image-URL"
-             onClick={this.props.viewBigImage}/>
+    <span className="ArtResult"
+      style={props.parent_state.displayArtResultImage}
+      >
+        <img src={props.parent_state.imageURL}
+          alt="random item from Cooper Hewitt collection based on search tag"
+          className="image-URL"
+          onClick={props.viewBigImage}
+        />
 
       </span>
     );
-  }
+
 }
 
 
