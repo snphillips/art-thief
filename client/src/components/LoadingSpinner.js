c927ab8834c71b2ba64e2ec3ba33879caf8bb215
Sarah Phillips <snphillips@gmail.com>
2021-12-14 22:02:04 -0500

    refactor LoadingSpinner from class to functional component

--- client/src/components/LoadingSpinner.js
+++ client/src/components/LoadingSpinner.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { css } from 'emotion';
-//react-spinners is an npm package
+// react-spinners is an npm package
 import { ScaleLoader } from 'react-spinners';
 
 const override = css`
@@ -9,19 +9,17 @@ const override = css`
     border-color: red;
 `;
 
-export default class LoadingSpinner extends React.Component {
-  render() {
-    return (
-      <span className='sweet-loading'>
-        <ScaleLoader
-          className={override}
-          sizeUnit={"px"}
-          // size={32}
-          height={32}
-          color={'#000'}
-          loading={this.props.loading}
-        />
-      </span>
-    )
-  }
+export default function LoadingSpinner(props) {
+  return (
+    <span className='sweet-loading'>
+      <ScaleLoader
+        className={override}
+        sizeUnit={"px"}
+        // size={32}
+        height={32}
+        color={'#000'}
+        loading={props.loading}
+      />
+    </span>
+  )
 }
